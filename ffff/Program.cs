41761d1e0db7762adbf69f71f42b9c4a9d9833b1
skaund <63222456+skaund@users.noreply.github.com>
2020-04-13 09:37:23 +0400

    Add files via upload

---
+++ ffff/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ListingPKS
+{
+    class Monster
+    {
+        public Monster()
+        {
+            this.health = 100;
+            this.ammo = 100;
+            this.name = "NoName";
+
+        }
+        public Monster(string name) : this()
+        {
+            this.name = name;
+        }
+        public Monster(int health, int ammo, string name)
+        {
+            this.health = health;
+            this.ammo = ammo;
+            this.name = name;
+        }
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                if (value > 0) health = value;
+                else health = 0;
+            }
+        }
+        public int Ammo
+        {
+            get
+            {
+                return ammo;
+            }
+            set
+            {
+                if (value > 0) ammo = value;
+                else ammo = 0;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public void Passport()
+        {
+            Console.WriteLine("Monster {0} \t health = {1} ammo = {2}",
+                name, health, ammo);
+        }
+        string name;
+        int health, ammo;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monster Masha = new Monster(200, 200, "Masha");
+            Masha.Passport();
+            --Masha.Health;
+            Masha.Ammo += 100;
+            Masha.Passport();
+            Console.Read();
+        }
+    }
+}
